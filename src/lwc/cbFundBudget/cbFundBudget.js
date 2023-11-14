c605f147164fb4e433beddcee2b4491fb366c6fa
CloudBudget Inc <68500411+CloudBudgetInc@users.noreply.github.com>
2023-11-14 13:11:34 +0100

    Balance budget in fund

--- src/lwc/cbFundBudget/cbFundBudget.js
+++ src/lwc/cbFundBudget/cbFundBudget.js
@@ -29,20 +29,28 @@ import getAnalyticsServer from '@salesforce/apex/CBGMFundPageController.getAnaly
 import getFundBudgetLinesServer from '@salesforce/apex/CBGMFundPageController.getFundBudgetLinesServer';
 import generateFundBudgetLinesServer from '@salesforce/apex/CBGMFundPageController.generateFundBudgetLinesServer';
 import saveFundAmountServer from '@salesforce/apex/CBGMFundPageController.saveFundAmountServer';
-import {_applyDecStyle, _message, _parseServerError, _getSOFromObject} from "c/cbUtils";
+import getAllocationGrantsServer from '@salesforce/apex/CBGMFundPageController.getAllocationGrantsServer';
+import {_applyDecStyle, _getCopy, _getSOFromObject, _message, _parseServerError} from "c/cbUtils";
 
 
 export default class CBFundBudget extends LightningElement {
 
 	@api recordId;
 	@track showSpinner = false;
-	@track budgetLines = [];
+	@track allCommittedBudgetLines = [];
+	@track allGrantBudgetLines = [];
+	@track grantBudgetLines = [];
 	@track selectedBYId;
 	@track budgetYearSO = [];
 	@track fundSO = [];
 	@track fundPlanLine = {
-		label: 'Committed Plan',
-		amounts: [],
+		Name: 'Committed Plan',
+		cb5__CBAmounts__r: [],
+		total: 0
+	};
+	@track fundBalanceLine = {
+		Name: 'Fund Balance',
+		cb5__CBAmounts__r: [],
 		total: 0
 	};
 
@@ -51,6 +59,10 @@ export default class CBFundBudget extends LightningElement {
 		_applyDecStyle();
 		await this.getAnalytics();
 		await this.getFundBudgetLines();
+		this.generateCommittedBudgetLine();
+		await this.getAllocationGrants();
+		this.generateGrantBudgetLines();
+		this.generateBalanceAmounts();
 	};
 
 	getAnalytics = async () => {
@@ -61,33 +73,27 @@ export default class CBFundBudget extends LightningElement {
 
 	getFundBudgetLines = async () => {
 		await getFundBudgetLinesServer({oppId: this.recordId})
-			.then(budgetLines => {
-				this.budgetLines = budgetLines;
-				this.prepareTable();
-			})
+			.then(budgetLines => this.allCommittedBudgetLines = budgetLines)
 			.catch(e => _parseServerError('Get Budget Lines Error : ', e))
 	};
 
-	generateBudgetLines = async () => {
-		this.showSpinner = true;
-		await generateFundBudgetLinesServer({oppId: this.recordId})
-			.error(e => _parseServerError('Generate Budget Lines Error : ' + e));
-		await this.getFundBudgetLines();
-		_message('success', 'Generated');
-		this.showSpinner = false;
+	getAllocationGrants = async () => {
+		await getAllocationGrantsServer({oppId: this.recordId})
+			.then(allGrantBudgetLines => this.allGrantBudgetLines = allGrantBudgetLines)
+			.catch(e => _parseServerError('Get Grant Allocation Error : ', e));
 	};
 
-	prepareTable = () => {
+	generateCommittedBudgetLine = () => {
 		try {
-			this.fundPlanLine.amounts = [];
+			this.fundPlanLine.cb5__CBAmounts__r = [];
 			this.fundPlanLine.total = 0;
-			if (!this.budgetLines || this.budgetLines.length === 0) return null;
-			if (!this.selectedBYId) this.selectedBYId = this.budgetLines[0].cb5__CBBudgetYear__c;
-			this.budgetLines.forEach(bl => {
+			if (!this.allCommittedBudgetLines || this.allCommittedBudgetLines.length === 0) return null;
+			if (!this.selectedBYId) this.selectedBYId = this.allCommittedBudgetLines[0].cb5__CBBudgetYear__c;
+			this.allCommittedBudgetLines.forEach(bl => {
 				bl.cb5__CBAmounts__r.forEach(amount => {
 					if (amount.cb5__CBPeriod__r.cb5__CBBudgetYear__c === this.selectedBYId) {
 						amount.label = amount.cb5__CBPeriod__r.Name;
-						this.fundPlanLine.amounts.push(amount);
+						this.fundPlanLine.cb5__CBAmounts__r.push(amount);
 						this.fundPlanLine.total += parseFloat(amount.cb5__Value__c);
 					}
 				})
@@ -98,6 +104,82 @@ export default class CBFundBudget extends LightningElement {
 	};
 
 	/**
+	 * Grant allocation grouped synthetic lines
+	 */
+	generateGrantBudgetLines = () => {
+		try {
+			const grantBudgetLinesBY = this.allGrantBudgetLines.filter(bl => bl.cb5__CBBudgetYear__c === this.selectedBYId);
+			const BLObject = {};
+			grantBudgetLinesBY.forEach(bl => {
+				console.log('bl = ' + JSON.stringify(bl));
+				let groupGrantBudgetLine = BLObject[bl.cb5__CBVariable2__c];
+				if (!groupGrantBudgetLine) {
+					groupGrantBudgetLine = {
+						Name: bl.cb5__CBVariable2__r.Name,
+						cb5__CBAmounts__r: _getCopy(bl.cb5__CBAmounts__r),
+						cb5__Value__c: 0
+					};
+					console.log('New groupGrantBudgetLine 1 = ' + JSON.stringify(groupGrantBudgetLine));
+					groupGrantBudgetLine.cb5__CBAmounts__r.forEach(a => a.cb5__Value__c = 0);
+					console.log('New groupGrantBudgetLine 2 = ' + JSON.stringify(groupGrantBudgetLine));
+					BLObject[bl.cb5__CBVariable2__c] = groupGrantBudgetLine;
+				}
+				bl.cb5__CBAmounts__r.forEach((amount, i) => {
+					groupGrantBudgetLine.cb5__CBAmounts__r[i].cb5__Value__c += +amount.cb5__Value__c;
+				});
+			});
+			this.grantBudgetLines = Object.values(BLObject);
+		} catch (e) {
+			_message('error', 'Generate Grant Budget Lines Error : ' + e);
+		}
+	};
+
+	generateBalanceAmounts = () => {
+		try {
+			const allBalanceAmounts = {}; // key is periodId, value is CB amount
+			let rest = 0;
+			const allGrantedAmountsMap = this.allGrantBudgetLines.reduce((r, bl) => { // key is grant amount period id. value is amount
+				bl.cb5__CBAmounts__r.forEach(a => {
+					let value = r[a.cb5__CBPeriod__c];
+					if (!value) value = 0;
+					r[a.cb5__CBPeriod__c] = +value + +a.cb5__Value__c;
+				});
+				return r;
+			}, {});
+			console.log('allGrantedAmountsMap = ' + JSON.stringify(allGrantedAmountsMap));
+			this.allCommittedBudgetLines.forEach(bl => {
+				bl.cb5__CBAmounts__r.forEach(amount => {
+					const balanceAmount = _getCopy(amount);
+					balanceAmount.class = 'dec';
+					allBalanceAmounts[amount.cb5__CBPeriod__c] = balanceAmount;
+					let granted = allGrantedAmountsMap[amount.cb5__CBPeriod__c] || 0;
+					console.log('Granted = ' + granted);
+					let balanceValue = rest + +amount.cb5__Value__c - granted;
+					console.log('balanceValue = ' + balanceValue);
+					balanceAmount.cb5__Value__c = balanceValue;
+					if (balanceValue < 0) balanceAmount.class += ' error';
+					rest = balanceValue;
+				});
+			});
+			console.log('allBalanceAmounts = ' + JSON.stringify(allBalanceAmounts));
+			this.fundBalanceLine.cb5__CBAmounts__r = [];
+			this.fundPlanLine.cb5__CBAmounts__r.forEach(planAmount => {
+				let balanceAmount = allBalanceAmounts[planAmount.cb5__CBPeriod__c];
+				this.fundBalanceLine.cb5__CBAmounts__r.push(balanceAmount);
+			});
+		} catch (e) {
+			_message('error', 'Generate Balance Line Error' + e);
+		}
+
+	};
+
+	///////////////// HANDLERS /////////////////////
+	handleFilter = async (event) => {
+		this.selectedBYId = event.target.value;
+		this.generateCommittedBudgetLine();
+		this.generateBalanceAmounts();
+	};
+	/**
 	 * Handler for lost amount
 	 */
 	saveAmount = async (event) => {
@@ -106,7 +188,7 @@ export default class CBFundBudget extends LightningElement {
 		if (!amountValue) amountValue = 0;
 		let amountNeedToBeSaved;
 		this.fundPlanLine.total = 0;
-		this.fundPlanLine.amounts.forEach(amount => {
+		this.fundPlanLine.cb5__CBAmounts__r.forEach(amount => {
 			if (amount.Id === amountId) {
 				amount.cb5__Value__c = amountValue;
 				amountNeedToBeSaved = amount;
@@ -115,12 +197,17 @@ export default class CBFundBudget extends LightningElement {
 		});
 
 		await saveFundAmountServer({amount: amountNeedToBeSaved}).catch(e => _parseServerError('Save Amount Error', e));
+		this.generateBalanceAmounts();
 	};
-
-	handleFilter = async (event) => {
-		this.selectedBYId = event.target.value;
-		this.prepareTable();
-	}
+	generateBudgetLines = async () => {
+		this.showSpinner = true;
+		await generateFundBudgetLinesServer({oppId: this.recordId})
+			.error(e => _parseServerError('Generate Budget Lines Error : ' + e));
+		await this.getFundBudgetLines();
+		_message('success', 'Generated');
+		this.showSpinner = false;
+	};
+	///////////////// HANDLERS /////////////////////
 
 
 }
